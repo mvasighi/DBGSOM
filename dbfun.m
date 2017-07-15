9b7d52be85c53957bc4379151f5e44c83615cd51
Mahdi Vasighi <vasighi@gmail.com>
2017-07-15 12:23:57 +0430

    Update dbfun.m

--- dbfun.m
+++ dbfun.m
@@ -1,5 +1,5 @@
 function DB=dbfun(data,net)
-% Calculate Davies–Bouldin index
+% Calculate Daviesâ€“Bouldin index
 
 % Directed Batch Growing Self Organizing Map (DBGSOM)
 % version 1.0 - July 2017
@@ -7,6 +7,22 @@ function DB=dbfun(data,net)
 % Institute for Advanced Studies in Basic Sciences, Zanjan, Iran
 % Department of Computer Science and Information Technology
 % www.iasbs.ac.ir/~vasighi/
+
+%     Directed Batch Growing Self Organizing Map (DBGSOM), version 1.0
+%     Copyright (C) 2017  Mahdi Vasighi
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 W=net.W;
 hitcount=net.hitcount;
