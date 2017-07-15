61d1fc8befda5ed79cf0aa4d35695d477d05f61b
Mahdi Vasighi <vasighi@gmail.com>
2017-07-15 12:25:49 +0430

    Update dbgsom.m

--- dbgsom.m
+++ dbgsom.m
@@ -9,6 +9,22 @@ function net=dbgsom(data,netset)
 % Department of Computer Science and Information Technology
 % www.iasbs.ac.ir/~vasighi/
 
+%     Directed Batch Growing Self Organizing Map (DBGSOM), version 1.0
+%     Copyright (C) 2017  Mahdi Vasighi
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 [n1,m1]=size(data);
 [data,pre_param]=prefun(data,'rs'); %data pre-processing
 data_tmp=data;
