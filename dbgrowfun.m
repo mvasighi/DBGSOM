90bb4ca9ee7a0d3c4df0d40a4f5415c95c0b5d65
Mahdi Vasighi <vasighi@gmail.com>
2017-07-15 12:25:18 +0430

    Update dbgrowfun.m

--- dbgrowfun.m
+++ dbgrowfun.m
@@ -8,6 +8,22 @@ function net=dbgrowfun(net,netset,ds,bo_id)
 % Department of Computer Science and Information Technology
 % www.iasbs.ac.ir/~vasighi/
 
+%     Directed Batch Growing Self Organizing Map (DBGSOM), version 1.0
+%     Copyright (C) 2017  Mahdi Vasighi
+% 
+%     This program is free software: you can redistribute it and/or modify
+%     it under the terms of the GNU General Public License as published by
+%     the Free Software Foundation, either version 3 of the License, or
+%     any later version.
+% 
+%     This program is distributed in the hope that it will be useful,
+%     but WITHOUT ANY WARRANTY; without even the implied warranty of
+%     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%     GNU General Public License for more details.
+% 
+%     You should have received a copy of the GNU General Public License
+%     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 W=net.W;
 Err=net.Err;
 grd=net.grd;
